43ff44f8a20590ec2770973811e1167b4870f306
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2021-07-21 18:35:25 -0500

    Update purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.html',],
+  purge: ['./index.html',],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
