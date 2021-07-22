84aa1a8de308f79f1112327c2f46781cf3890ca6
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2021-07-22 01:55:55 -0500

    Update landing page UI and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ['./index.html',],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+    },
+    screens: {
+      'xs': '320px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         teal: colors.teal,
         cyan: colors.cyan,
+        'blue-gray': colors.blueGray,
       },
     },
   },
